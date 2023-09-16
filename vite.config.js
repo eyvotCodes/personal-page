d82cf13fb4c4e0df8a0953d7241d7497f7e55604
Fabián Leyva <fleyva.home@outlook.com>
2023-09-16 11:25:41 -0600

    add initial 404 page

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite"
+import { resolve } from 'path'
 import pugPlugin from "vite-plugin-pug"
 
 
 export default defineConfig({
   build: {
-    target: "es2015"
+    target: "es2015",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, '404.html'),
+      },
+    },
   },
   plugins: [
-    pugPlugin()
-  ]
+    pugPlugin(),
+  ],
 });
